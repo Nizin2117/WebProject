820e89829f8ac8a395ce3feabed12977daf912db
Nizin2117 <2023_cete_ladionanhi@online.htcgsc.edu.ph>
2025-04-27 12:00:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const heading = document.getElementById('heading');
+const button = document.getElementById('changeTextButton');
 
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    heading.textContent = 'You clicked the button!'; // Change the heading text
+});

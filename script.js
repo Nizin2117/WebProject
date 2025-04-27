c5973b9c3967a2dded09546eb5c9f71ec2af3d09
Nizin2117 <2023_cete_ladionanhi@online.htcgsc.edu.ph>
2025-04-27 12:22:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const button = document.getElementById('changeTextButton');
 
 // Add an event listener to the button
 button.addEventListener('click', function() {
-    heading.textContent = 'You clicked the button!'; // Change the heading text
-});
+    heading.textContent = 'Hello, World!'; 
+    heading.style.fontSize = '3em'; // Change the heading size

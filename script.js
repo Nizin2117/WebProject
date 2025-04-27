0b7e3c53b41d5db0f88c4a63e5143520d5de2cd8
Nizin2117 <2023_cete_ladionanhi@online.htcgsc.edu.ph>
2025-04-27 12:24:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const heading = document.getElementById('heading');
 const button = document.getElementById('changeTextButton');
 
-// Add an event listener to the button
 button.addEventListener('click', function() {
     heading.textContent = 'Hello, World!'; 
     heading.style.fontSize = '3em'; // Change the heading size
+    heading.style.color = '#ff6347'; // Change the heading color to red
+});

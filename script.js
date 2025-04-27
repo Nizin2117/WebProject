e81a15e0105a005465eecff00e08649e1c6e4709
Nizin2117 <2023_cete_ladionanhi@online.htcgsc.edu.ph>
2025-04-27 12:37:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const heading = document.getElementById('heading');
 const button = document.getElementById('changeTextButton');
 
+// Add an event listener to the button
 button.addEventListener('click', function() {
-    heading.textContent = 'Get Ready to Learn, How to Code!'; //text change
+    heading.textContent = 'Get Ready to Learn, How to Code!'; // Change the heading text
 });
